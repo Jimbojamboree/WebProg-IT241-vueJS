1f1d8bab1a51a572f008b7281d1d87ca7d81099d
John Michael Corpuz <jccorpuz@student.apc.edu.ph>
2026-02-03 13:52:49 +0000

    updated app.vue caps fix

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <h1>Food</h1>
-  <food-item/>
-  <food-item/>
-  <food-item/>
-  <Comment-form/>
-  <comment/>
+  <FoodItem/>
+  <FoodItem/>
+  <FoodItem2/>
+  <CommentForm/>
+  <Comment/>
    
 
 </template>
